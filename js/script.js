711679f4485c9682a9d35fdbc651b8f782183f38
Kolgute <kolgute@mail.ru>
2022-06-09 14:07:16 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function Openslider(cl,ad){
+			var slides = document.getElementsByClassName(cl);
+			slides[0].classList.add(ad);
+		}
+function Closeslider(cl,ad){
+			var slides = document.getElementsByClassName(cl);
+			slides[0].classList.remove(ad);
+		}
